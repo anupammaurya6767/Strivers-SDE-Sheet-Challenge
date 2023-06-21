de96616b40676e4288b21a2b65a5997c53752763
Anupam Maurya <anupammaurya6767@gmail.com>
2023-06-21 09:03:33 +0530

    Create Find Pattern in String - KMP Algorithm.cpp

---
+++ Find Pattern in String - KMP Algorithm.cpp
@@ -0,0 +1,19 @@
+// https://www.youtube.com/watch?v=V5-7GzOfADQ
+
+bool findPattern(string p, string s)
+{
+    int m=p.size(), n=s.size();
+    int count=0;
+   for(int i=0;i<n;i++){
+       string str=s.substr(i,m);
+       if(str==p){
+           count++;
+       }
+       else{
+          continue;
+       }
+   }
+    if(count>0) return true;
+    else if(count==0) return false;
+    
+}
